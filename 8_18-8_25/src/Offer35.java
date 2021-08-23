658ddccadf451044c181925cb09f0456bd932301
shishc9 <52554717+shishc9@users.noreply.github.com>
2021-08-23 14:30:38 +0800

    8-23

---
+++ 8_18-8_25/src/Offer35.java
@@ -0,0 +1,38 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+
+public class Offer35 {
+    public Node copyRandomList(Node head) {
+        if(head == null) {
+            return null;
+        }
+
+        Node cur = head;
+        Map<Node, Node> map = new HashMap<>();
+        while(cur != null) {
+            map.put(cur, new Node(cur.val));
+            cur = cur.next;
+        }
+
+        cur = head;
+        while(cur != null) {
+            map.get(cur).next = map.get(cur.next);
+            map.get(cur).random = map.get(cur.random);
+            cur = cur.next;
+        }
+
+        return map.get(head);
+    }
+}
